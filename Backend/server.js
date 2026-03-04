a1884159850077e1fc2772e9c27d276d1f884c74
ykahsahde <yashkhade.22@stvincentngp.edu.in>
2026-03-04 17:02:41 +0530

    changes

--- Backend/server.js
+++ Backend/server.js
@@ -22,7 +22,7 @@ mongoose.connect(process.env.MONGO_URI)
 })
 app.use(express.json());
 app.use(cors({
-  origin: "http://localhost:5173"
+  origin: "https://frelance-ai.vercel.app"
 }));
 // Routes
 app.use("/api/auth",authRoutes);
